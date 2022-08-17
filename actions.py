57dfa4143add9e3aa098d8392fb53bcbd4187d48
vamshi2315 <109863639+vamshi2315@users.noreply.github.com>
2022-08-17 14:00:00 +0530

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,9 +1,14 @@
+#The following are the actions that can be performed in the program
+
+#Let’s code the view data block.
+
 def view_data(username):
 	ff = open(username, 'r')
 	print(ff.read())
 	ff.close()
 
-
+#To code the task add block we need to keep in mind the basic concepts of text handling in python.
+#We will ask the user how many tasks he/she wants to add and as per his wish we will iterate a loop for that many times and ask him to enter his task and the target by which he wants to finish the task.
 def task_add(username):
 	print("Please enter the number of tasks you want to add")
 	j = int(input())
@@ -27,7 +32,8 @@ def task_add(username):
 			break
 	f1.close()
 
-
+#Updating the task status goes in the similar concept of text handling in python.
+#What we will be doing is, we will save which task number is completed, which is ongoing, and which is not yet started with a date time stamp with them.
 def task_update(username):
 	username = username + " TASK.txt"
 	print("Please enter the tasks which are completed ")
@@ -53,7 +59,7 @@ def task_update(username):
 	fw.write(task_not_started)
 	fw.write("\n")
 
-
+#Now we are left with the task status function. This function is as simple as the ‘view_data’ function.
 def task_status(username):
 	ussnm = username+" TASK.txt"
 	o = open(ussnm, 'r')
