e75d9225b37e7229b82cd0e05f558bcbcaed31dd
vamshi2315 <109863639+vamshi2315@users.noreply.github.com>
2022-08-17 13:51:26 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,4 +1,6 @@
 def welcome():
+	#welcome message
+	#option to signup and login
 	print("WELCOME TO PERSONALIZED TASK MANAGER\n\
 	1--Signup\n\
 	2--Login")
@@ -8,5 +10,5 @@ def welcome():
 	elif a == 2:
 		login()
 	else:
-		print("Oops!\nSeems like you've entered wrong input.\nPlease select from above options i.e, 1 or 2")
-		welcome()
+		print("Oops!\nSeems like you've entered wrong input.\nPlease select from above options i.e, 1 or 2")	#warning if wrong credentials are entered
+		welcome()	#lets user to try again
